38703e0e0f493a98aa03ce97e4507446f1bc19ed
EricB-59 <ebaenac59@gmail.com>
2024-06-20 14:51:58 +0100

    Eric

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienciesComponent } from './experiencies/experiencies.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PresentationComponent],
+  imports: [RouterOutlet, PresentationComponent, AboutComponent, ExperienciesComponent, ProjectsComponent, TechnologiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/experiencies/experiencies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencies',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiencies.component.html',
+  styleUrl: './experiencies.component.css'
+})
+export class ExperienciesComponent {
+
+}
